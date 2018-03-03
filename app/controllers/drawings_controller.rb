c92a5a7d3b298958443526a5c5e0e1a644cddb9d
Filip Tatarnik <ftatarnik@gmail.com>
2018-03-02 19:38:12 -0600

    Second commit, added image capability

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -69,6 +69,6 @@ class DrawingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def drawing_params
-      params.require(:drawing).permit(:name)
+      params.require(:drawing).permit(:name, :avata)
     end
 end
--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,4 +1,4 @@
 class Drawing < ActiveRecord::Base
-  has_attached_file :drawing, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :drawing, content_type: /\Aimage\/.*\z/
+  has_attached_file :avata, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avata, content_type: /\Aimage\/.*\z/
 end
